38849f4820e22a991f705895940b2e4258e4194e
os-studio <neonuts@gmail.com>
2022-01-22 13:20:10 -0300

    New html code and CSS

--- js/main.js
+++ js/main.js
@@ -90,19 +90,37 @@
 //         img.src = imgsrc;
 //         imgContainer.appendChild(img);
 //     }
+let currentDay = new Date();
+let year = currentDay.getFullYear();
+let footer = document.querySelector('footer');
+// var video1 = document.getElementById("homeVideo1");
+// var video2 = document.getElementById("homeVideo2");
+// var video3 = document.getElementById("homeVideo3");
+// var video4 = document.getElementById("homeVideo4");
+// video1.loop = true;
+// video1.play();
+// video2.loop = true;
+// video2.play();
+// video3.loop = true;
+// video3.play();
+// video4.loop = true;
+// video4.play();
+function clampBuilder( minWidthPx, maxWidthPx, minFontSize, maxFontSize ) {
+    const root = document.querySelector( "html" );
+    const pixelsPerRem = Number( getComputedStyle( root ).fontSize.slice( 0,-2 ) );
+  
+    const minWidth = minWidthPx / pixelsPerRem;
+    const maxWidth = maxWidthPx / pixelsPerRem;
+  
+    const slope = ( maxFontSize - minFontSize ) / ( maxWidth - minWidth );
+    const yAxisIntersection = -minWidth * slope + minFontSize
+  
+    return `clamp( ${ minFontSize }rem, ${ yAxisIntersection }rem + ${ slope * 100 }vw, ${ maxFontSize }rem )`;
+  }
 
-var video1 = document.getElementById("homeVideo1");
-var video2 = document.getElementById("homeVideo2");
-var video3 = document.getElementById("homeVideo3");
-var video4 = document.getElementById("homeVideo4");
-video1.loop = true;
-video1.play();
-video2.loop = true;
-video2.play();
-video3.loop = true;
-video3.play();
-video4.loop = true;
-video4.play();
-
+const setYear = function() {
+    footer.innerText = `Website developed by ocampo santiago ${year}`
+}
+setYear();
 // }
 // window.addEventListener("load", init, false);
