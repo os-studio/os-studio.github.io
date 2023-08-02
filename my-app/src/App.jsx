1e4f9326f8ab04b94f72403079635d49ac5c01c0
os-studio <neonuts@gmaiil.com>
2023-08-02 13:43:51 -0300

    new update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,9 +8,6 @@ function App() {
     <div className='containerContent'>
       <NavBar />
       <MyHome />
-      <MyWork />
-      <MyAbout />
-      <MyContact />
       </div>
     </>
   )
@@ -19,11 +16,10 @@ function NavBar () {
   return (
     <>
  <nav className="containerNav">
-      <div className='os'>_OS</div>
-      <div className='buttonNav'><a href="#">Home</a></div>
-      <div className='buttonNav'><a href="#">Work</a></div>
-      <div className='buttonNav'><a href="#">About</a></div>
-      <div className='buttonNav'><a href="#">Contact</a></div>
+      <div className='os'>OS</div>
+      <div className='buttonNav'>[version 1.0]</div>
+      <div className='buttonNav'>TextureSize: 1440,1024</div>
+
     </nav>
     </>
   )
@@ -40,32 +36,6 @@ function MyHome (){
     </>
   )
 }
-function MyWork (){
-  return(
-    <>
-      <div className='containerWork'>
-        <h1>Work</h1>
-      </div>
-    </>
-  )
-}
-function MyAbout (){
-  return(
-    <>
-      <div className='containerAbout'>
-        <h1>About</h1>
-      </div>
-    </>
-  )
-}
-function MyContact (){
-  return(
-    <>
-      <div className='containerContact'>
-        <h1>Contact</h1>
-      </div>
-    </>
-  )
-}
+
 
 export default App
